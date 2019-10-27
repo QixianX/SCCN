7b29d5caf6cf3b6a1fb1edd91c50f4f9f74f98ee
QixianX <1037916520@qq.com>
2019-10-27 10:48:54 +0800

    Update sccn_test_app.py

--- sccn_test_app.py
+++ sccn_test_app.py
@@ -4,8 +4,12 @@ import DAEk
 import cv2
 import sys
 import time
-def SCCN(opt_path,sar_path,save_path,noise_std,p_times,p_step,lam,t_times,t_step,TEXT):
-
+def SCCN(opt_path,sar_path,save_path,noise_std,p_times,p_step,lam,t_times,t_step,info):
+    
+    global map
+    global phi
+    global shape
+    TEXT = info
     img_opt=cv2.imread(opt_path,0)
     shape =img_opt.shape
     img_opt = (img_opt.astype(np.float32)/255).reshape([1,shape[0],shape[1],1])
@@ -13,50 +17,48 @@ def SCCN(opt_path,sar_path,save_path,noise_std,p_times,p_step,lam,t_times,t_step
 
     k=10000#60000
     map = np.array(np.random.rand(shape[0],shape[1]),dtype=np.float32)
-    global map
-    global phi
-    global shape
-    print(map.shape)
+
+    #print(map.shape)
     ksize = 3
     phi = lam
     now = time.strftime("\n%d-%H:%M:%S  ", time.localtime())
     start_time = time.localtime()
-    TEXT.AppendText(now+':start pretrain\n')
+    #TEXT.AppendText(now+':start pretrain\n')
 
     #--------------------------------------鍒濆鍖�---------------------------------------------#
     a=DAEk.DAE(TEXT)
     now = time.strftime("\n%d-%H:%M:%S  ", time.localtime())
-    TEXT.AppendText(now + ':pretrain : 1st opt_layer\n')
+    #TEXT.AppendText(now + ':pretrain : 1st opt_layer\n')
     a.set_par(img_opt,1,20,name='conv_opt_1',k_size=ksize,step=p_step,times=p_times,std=noise_std)
     [opt_1_w,opt_1_b,opt_1_h] = a.par()
     a.kill()
 
     now = time.strftime("\n%d-%H:%M:%S  ", time.localtime())
-    TEXT.AppendText(now + ':pretrain : 1st sar_layer\n')
+    #TEXT.AppendText(now + ':pretrain : 1st sar_layer\n')
     a.set_par(img_sar,1,20,name='conv_sar_1',is_mul=1,k_size=ksize,step=p_step,times=p_times,std=noise_std)
     [sar_1_w,sar_1_b,sar_1_h] = a.par()
     a.kill()
 
     now = time.strftime("\n%d-%H:%M:%S  ", time.localtime())
-    TEXT.AppendText(now + ':pretrain : 2nd opt_layer\n')
+    #TEXT.AppendText(now + ':pretrain : 2nd opt_layer\n')
     a.set_par(opt_1_h,20,20,name='conv_opt_2',k_size=1,step=p_step,times=p_times,std=noise_std)
     [opt_2_w,opt_2_b,opt_2_h] = a.par()
     a.kill()
 
     now = time.strftime("\n%d-%H:%M:%S  ", time.localtime())
-    TEXT.AppendText(now + ':pretrain : 2nd sar_layer\n')
+    #TEXT.AppendText(now + ':pretrain : 2nd sar_layer\n')
     a.set_par(sar_1_h,20,20,name='conv_sar_2',is_mul=1,k_size=1,step=p_step,times=p_times,std=noise_std)
     [sar_2_w,sar_2_b,sar_2_h] = a.par()
     a.kill()
 
     now = time.strftime("\n%d-%H:%M:%S  ", time.localtime())
-    TEXT.AppendText(now + ':pretrain : 3rd opt_layer\n')
+    #TEXT.AppendText(now + ':pretrain : 3rd opt_layer\n')
     a.set_par(opt_2_h,20,20,name='conv_opt_3',k_size=1,step=p_step,times=p_times,std=noise_std)
     [opt_3_w,opt_3_b,opt_3_h] = a.par()
     a.kill()
 
     now = time.strftime("\n%d-%H:%M:%S  ", time.localtime())
-    TEXT.AppendText(now + ':pretrain : 3rd sar_layer\n')
+    #TEXT.AppendText(now + ':pretrain : 3rd sar_layer\n')
     a.set_par(sar_2_h,20,20,name='conv_sar_3',is_mul=1,k_size=1,step=p_step,times=p_times,std=noise_std)
     [sar_3_w,sar_3_b,sar_3_h] = a.par()
     a.kill()
@@ -92,11 +94,11 @@ def SCCN(opt_path,sar_path,save_path,noise_std,p_times,p_step,lam,t_times,t_step
     #---------------4
     l = DAEk.last()
     now = time.strftime("\n%d-%H:%M:%S  ", time.localtime())
-    TEXT.AppendText(now + ':pretrain : last layer\n')
+    #TEXT.AppendText(now + ':pretrain : last layer\n')
     [opt_4_w,opt_4_b,sar_4_w,sar_4_b,map_temp] = l.set_par(opt_3_h,sar_3_h,20,20)
 
     now = time.strftime("\n%d-%H:%M:%S  ", time.localtime())
-    TEXT.AppendText(now + ':pretrain :over\n')
+    #TEXT.AppendText(now + ':pretrain :over\n')
     print(map_temp)
     #nor
     map_temp = map_temp.reshape(shape)
@@ -140,12 +142,12 @@ def SCCN(opt_path,sar_path,save_path,noise_std,p_times,p_step,lam,t_times,t_step
     global_steps = t_times # 鎬荤殑杩唬娆℃暟
     decay_steps = 100  # 琛板噺娆℃暟
     global_ = tf.Variable(tf.constant(0))
-    learning_rate = tf.train.exponential_decay(step,global_,decay_steps=global_steps,decay_rate=decay_rate,staircase=False)
+    learning_rate = tf.compat.v1.train.exponential_decay(step,global_,decay_steps=global_steps,decay_rate=decay_rate,staircase=False)
 
 
     opt = tf.train.AdamOptimizer(learning_rate).minimize(loss)#,global_step = global_)
     now = time.strftime("\n%d-%H:%M:%S  ", time.localtime())
-    TEXT.AppendText(now + ':train\n')
+    #TEXT.AppendText(now + ':train\n')
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
 
@@ -155,7 +157,7 @@ def SCCN(opt_path,sar_path,save_path,noise_std,p_times,p_step,lam,t_times,t_step
             cv2.imwrite(save_path +'/before_' +str(i)  + '_opt.tif', ((sess.run(tf.reshape(opt3h[:,:,:,i],shape))) * 255).astype(np.uint8))
             cv2.imwrite(save_path +'/before_' +str(i)  + '_sar.tif', ((sess.run(tf.reshape(sar3h[:,:,:,i],shape))) * 255).astype(np.uint8))
         temp = 0
-        TEXT.AppendText('=')
+        #TEXT.AppendText('=')
         for j in range(global_steps):
             cal_map(sess.run(sub))
             sess.run(opt,feed_dict={map_p:map})
@@ -163,7 +165,7 @@ def SCCN(opt_path,sar_path,save_path,noise_std,p_times,p_step,lam,t_times,t_step
             if(j*10//global_steps == temp):
                 pass
             else:
-                TEXT.AppendText('=')
+                #TEXT.AppendText('=')
                 temp = j*10//global_steps
 
             phi = phi / 1
@@ -177,4 +179,4 @@ def SCCN(opt_path,sar_path,save_path,noise_std,p_times,p_step,lam,t_times,t_step
             cv2.imwrite(save_path+ '/after_'+str(i)  + '_sar.tif', ((sess.run(tf.reshape(sar4h[:,:,:,i],shape))) * 255).astype(np.uint8))
         print('\n\n\n\n\n\n\n\n')
     now = time.strftime("\n%d-%H:%M:%S  ", time.localtime())
-    TEXT.AppendText(now + ':Success!\n')
+    #TEXT.AppendText(now + ':Success!\n')
